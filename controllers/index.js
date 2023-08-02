8c5f8c265426654ae98a51365b62739bb8695468
Emma Butler <130527417+butlerem@users.noreply.github.com>
2023-08-02 16:20:06 -0400

    edit filepathname

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const userRoutes = require('/userRoutes'); // make sure the path is correct
+const userRoutes = require('./userroutes'); // make sure the path is correct
 
 // Use the user routes under the '/users' endpoint
 router.use('/users', userRoutes);
