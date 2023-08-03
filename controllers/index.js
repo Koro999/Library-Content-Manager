19558ac1ebe82a7446c97d4ff19061f51ac00ae0
Chris Dang <128331826+Koro999@users.noreply.github.com>
2023-08-02 20:46:21 -0400

    Delete controllers directory

--- controllers/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-const userRoutes = require('/userRoutes'); // make sure the path is correct
-
-// Use the user routes under the '/users' endpoint
-router.use('/users', userRoutes);
-
-module.exports = router;
